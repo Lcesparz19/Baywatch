ee10fa765957c9ff9e406bebff4dfe09f2cf2c6f
Lcesparz19 <lucacruztd@gmail.com>
2017-06-28 16:11:14 -0400

    Added flick constant

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const app = {
     init : function(){
+        this.max=0
         document
         .querySelector('form#flick-form')
-        .addEventListener('submit', this.handleSubmit)
+        .addEventListener('submit', this.handleSubmit.bind(this))
     },
 
     handleSubmit: function(ev){
         ev.preventDefault()
         const f = ev.target
-        console.log(f.flickName.value)
+        const flick ={
+            id: this.max + 1,
+            name: f.flickName.value,
+
+        }
+        console.log(flick)
+        this.max ++
+        
     },
 }
 
-app.init()
+app.init('form#flick-form')
