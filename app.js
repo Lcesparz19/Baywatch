bbc292a93757e1c9e206e7b9dd0ba9e49f3dbeb7
Lcesparz19 <lucacruztd@gmail.com>
2017-06-28 23:24:06 -0400

    Added array as well as DOM

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 const app = {
-    init : function(){
-        this.max=0
-        document
-        .querySelector('form#flick-form')
-        .addEventListener('submit', this.handleSubmit.bind(this))
-    },
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
 
-    handleSubmit: function(ev){
-        ev.preventDefault()
-        const f = ev.target
-        const flick ={
-            id: this.max + 1,
-            name: f.flickName.value,
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener(
+        'submit', 
+        this.handleSubmit.bind(this)
+      )
+  },
 
-        }
-        console.log(flick)
-        this.max ++
-        
-    },
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    this.flicks[flick.id - 1] = flick.name
+    
+    return item
+  },
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+    }
+
+    const listItem = this.renderListItem(flick)
+    this.list.appendChild(listItem)
+
+    this.max ++
+  },
 }
 
-app.init('form#flick-form')
+app.init({
+  formSelector: 'form#flick-form',
+  listSelector: '#flick-list',
+})
